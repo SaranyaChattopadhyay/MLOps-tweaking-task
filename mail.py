bd21bdb3e62aee4c9e388a02ff2443b0f761e725
SaranyaChattopadhyay <55938071+SaranyaChattopadhyay@users.noreply.github.com>
2020-05-26 02:08:22 +0530

    mail commit

---
+++ mail.py
@@ -0,0 +1,12 @@
+import smtplib
+rcvr = "sutirtha16@gmail.com"
+sender = "chattopadhyaysaranya@gmail.com"
+msg = "Hey Developer, your model has greater than 96% accuracy and is ready to use for predictions!!" 
+pswd = "24052001"
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.ehlo()
+server.starttls()
+server.login(sender,pswd)
+print("Access Granted")
+server.sendmail(sender,recvr,msg)
+print("Mail sent!!")
